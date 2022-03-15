5cd9bd603d59fd3c44dba8bd20b662f1f7267b26
Jorge Loor <81595290+theerudito@users.noreply.github.com>
2022-03-15 17:51:29 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+const ham = document.querySelector(".ham");
+const enlaces = document.querySelector(".enlaces-menu");
+const barras = document.querySelectorAll(".ham span");
+
+ham.addEventListener("click", () => {
+	enlaces.classList.toggle("activado");
+	barras.forEach((e) => {
+		e.classList.toggle("animado");
+	});
+	ham.classList.toggle("girar");
+});
